58a9816713e0da2e8a685a3d0246a83612900121
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2024-07-15 19:29:12 -0500

    Create HolaMundo-JulianB0315.py

---
+++ Ejercicios/01 Hola mundo/HolaMundo-JulianB0315.py
@@ -0,0 +1,12 @@
+#Mi saludo 
+print('Hola mundo soy Julian')
+#los 4 tipos de prints
+print('Tipo str')
+print(type('Hola que tal'))
+print('Tipo bool')
+print(type(True))
+print('Tipo int')
+print(type(30))
+print('Tipo list')
+print(type([30, 40]))
+
