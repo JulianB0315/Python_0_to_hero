d60437bf7c67ce63cb6065e654ff992b4d683121
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2024-07-16 10:45:20 -0500

    Update 01 print('Hola mundo') .py

--- Clases/01 print('Hola mundo') .py
+++ Clases/01 print('Hola mundo') .py
@@ -23,6 +23,4 @@ print(type([1,2,3])) #Tipo "list"
 print(type(["Hola","mundo"])) #Tipo "list"
 print(type({'Nombre':'Julian'})) #Tipo "dict"
 print(type((1.3, 1.4, 1.5))) #Tipo "tuple"
-print(type({'Nombre':'Julian'})) #Tipo "dict"
-print(type(True)) #Tipo "bool"
 #Ahora ya estas list@ para el primer Modulo "01 Hola Mundo" https://github.com/JulianB0315/Python_0_to_hero/blob/main/Ejercicios/01%20Hola%20mundo/01%20Hola%20mundo.md
