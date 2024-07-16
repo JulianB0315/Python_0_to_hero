904cc20c9a691af4c00a3051fae72c317221593b
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2024-07-16 16:27:58 -0500

    Update 03 _1+'1'=11 .py

--- Clases/03 _1+'1'=11 .py
+++ Clases/03 _1+'1'=11 .py
@@ -1,4 +1,21 @@
+#-------------------------------------------------------------------------------------------------------------------------------------------
 _1='Respuesta 1'
 print(_1+'1')
-#Operadores 
-
+#-------------------------------------------------------------------------------------------------------------------------------------------
+#Operadores aritméticos los cuales son :
+print('Suma')
+print(1 + 2)
+print('Resta')
+print(2 - 1)
+print('Multiplicación')
+print(2 * 3)
+print('División')
+print(9 / 3)#Poniendo una solo / la división solo dará decimal si lo necesita 
+print(7 / 2)#Como en este caso siendo la operación más exacta  
+print(7 // 2)#Poniendo dos veces /  la división no tendrá decimal y será redondeada 
+print(10 // 3)
+print('Residuos de la división')
+print(3 % 2) #Muestra los residuos de una división                         
+print('Potencia')
+print(3 ** 2)  
+#-------------------------------------------------------------------------------------------------------------------------------------------
