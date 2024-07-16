b35bbfa842deb0e84ccb4d818515a853d2b03e2a
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2024-07-16 10:53:08 -0500

    Create Cambiosdevariables-JulianB0315.py

---
+++ Ejercicios/02 Cambios de variables/Cambiosdevariables-JulianB0315.py
@@ -0,0 +1,35 @@
+#Actividad 1
+nombre1='Hola soy Julian tengo'
+edad=19
+carrera='años y estudio Ingeniería en software '
+pregunta='¿Este dato es?'
+dato=True 
+
+print(nombre1,edad,carrera,pregunta,dato)
+
+#Actividad 2
+
+num1=3.14
+list1=[30,40,50]
+bool1=False
+
+print(type(num1))
+print(type(list1))
+print(type(bool1))
+
+str_num1=str(num1)
+print(type(str_num1))
+
+str_list1=str(list1)
+print(type(str_list1))
+
+str_bool1=str(bool1)
+print(type(str_bool1))
+
+#Actividad 3
+nombre2=input('¿Cómo te llamas?')
+años=input('¿Cuál es tu edad?')
+comida=input('¿Cuál es tu comida favorita?')
+
+print('Hola',nombre2,'de',años,'edad')
+print('Te gusta el',comida,'que delicioso!!')
