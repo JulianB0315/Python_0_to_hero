1b03a22edb64859b44331f4b7cb9c95f2e99137a
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2024-07-19 15:59:09 -0500

    Create 04 Strings.py

---
+++ Clases/04 Strings.py
@@ -0,0 +1,8 @@
+#El texto es un tipo de datos de cadena. Cualquier tipo de datos escrito como texto es una cadena. 
+#Cualquier dato bajo cotización simple, doble o triple son cadenas.
+#Existen diferentes métodos de cadena y funciones integradas para tratar los tipos de datos de cadena.
+#Para verificar la longitud de una cadena, use el método len().
+#Creamos una variable
+string="A"
+print(string)#Imprime solo el valor de variable, pero agregando la función len()  se usa para contar las letras de str
+print(len(string))
