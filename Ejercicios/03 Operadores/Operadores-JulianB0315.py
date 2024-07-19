d84fc6c0c576c2797fe4e3962a35b5aae32df966
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2024-07-19 15:14:07 -0500

    renombro ficheros y arreglo cosas

--- Ejercicios/03 Operadores/Operadores-JulianB0315.py
+++ Ejercicios/03 Operadores/Operadores-JulianB0315.py
@@ -1,10 +1,10 @@
 #Crea una calculadora que sume, reste, multiplique y divida variables posteriormente introducidas por el usuario.
-n1=float(input('Introduce el valor de n1: '))
-n2=float(input('Introduce el valor de n2: '))
+n1=30.4
+n2=40,5
 print('La suma de n1 mas n2 es ',n1+n2)
 print('La resta de n1 menos n2 es ',n1-n2)
 print('La división de n1 entre n2 es ',n1/n2)
 print('La multiplicación de n1 por10 n2 es ',n1*n2)
 #Multiplica una cadena de texto dependiendo las veces que sea el usuario
-n3=int(input('¿Cuántas veces deseas multiplicar hola?'))
+n3=5
 print(' Hola '*n3)
