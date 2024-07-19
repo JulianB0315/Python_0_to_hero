d85388c43f7f9a0fa4ccb3721c8aca26d44a4ebf
Diego Castro <diegoalbertocastropastor@gmail.com>
2024-07-19 18:50:50 -0500

    Create Operadores-Diego17cp.py

---
+++ Ejercicios/03 Operadores/Operadores-Diego17cp.py
@@ -0,0 +1,10 @@
+# Ejercicio 1
+num1=20
+num2=10
+print("La suma de ambos números es",num1+num2)
+print("La resta de ambos números es",num1-num2)
+print("La multiplicación de ambos números es",num1*num2)
+print("La división de ambos números es",num1/num2)
+# Ejercicio 2
+multiplicador=10
+print("Hola mundo "*multiplicador)
