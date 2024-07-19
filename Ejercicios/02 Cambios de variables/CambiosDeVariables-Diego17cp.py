3722ea676c817f4cf6f0864bdb02f0191149c44b
Diego Castro <diegoalbertocastropastor@gmail.com>
2024-07-19 15:18:22 -0500

    Create CambiosDeVariables-Diego17cp.py

---
+++ Ejercicios/02 Cambios de variables/CambiosDeVariables-Diego17cp.py
@@ -0,0 +1,21 @@
+# Ejercicio 1
+nombre="Diego"
+edad=18
+pais="Perú"
+carrera="Ing. Software"
+print("Hola, soy "+nombre+", soy de "+pais+", tengo "+str(edad)+" años y estudio la carrera de "+carrera)
+# Ejercicio 2
+boolean=True
+cambio=3.74
+lenguajes=3
+boolean_str=str(boolean)
+cambio_str=str(cambio)
+lengujes=str(lenguajes)
+print(type(lenguajes))
+print(type(cambio))
+print(type(boolean))
+# Ejercicio 3
+name=input("¿Cuál es tu nombre?")
+langfav=input("¿Cuál es tu lenguaje de programación favorito?")
+comidafav=input("¿Cuál es tu comida favorita?")
+print("Me llamo "+name+", mi lenguaje de programación favorito es JavaScript y mi comida favorita es el"+comidafav)
