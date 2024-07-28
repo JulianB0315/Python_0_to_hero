1a26be9a44c93f5d25a97c92f8473c2c3f5bab32
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2024-07-27 19:33:59 -0500

    Update 05 Listas.py

---
+++ Clases/05 Listas.py
@@ -0,0 +1,8 @@
+#Anteriormente vimos las listas, pero solo como un tipo de dato, pero ¿Qué son las listas y cuáles son?
+#Hay cuatro tipos de datos de recopilación en Python
+#Estos son list,Tuple,Set,Dictionary
+#List: es una colección que está ordenada y cambiante(modificable). Permite miembros duplicados.
+#Tuple: es una colección ordenada e inmutable o no modificable(inmutable). Permite miembros duplicados.
+#Set: es una colección que no está ordenada, no está indexada y no es modificable, pero podemos agregar nuevos elementos al conjunto. No se permiten miembros duplicados.
+#Dictionary:es una colección que es desordenada, cambiante(modificable) e indexada. Sin miembros duplicados.
+#Una lista es la recopilación de diferentes tipos de datos que están ordenados y modificables(mutables). Una lista puede estar vacía o puede tener diferentes elementos de tipo de datos.
