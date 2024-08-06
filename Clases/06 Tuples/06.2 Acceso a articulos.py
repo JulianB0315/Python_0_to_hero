0c5491c6951f6ff1998c7526f2091fd6d6117371
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2024-08-06 15:03:22 -0500

    Create 06.2 Acceso a articulos.py

---
+++ Clases/06 Tuples/06.2 Acceso a articulos.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------------------------------------------------------------------
+#Indexación Positiva
+# Similar al tipo de datos de lista, utilizamos la indexación positiva o negativa para acceder a los elementos de tupla.
+frutas = ('platano', 'naranja', 'mango', 'manzana')
+#            0           1         2         3
+arti_1=frutas[0]
+print(arti_1)
+arti_2=frutas[1]
+print(arti_2)
+arti_3=len(frutas)-1
+print(arti_3)
+#-------------------------------------------------------------------------------------------------------------------------------------------
+#Indexación negativa 
+#Medios de indexación negativos que comienzan desde el final, 
+#-1 se refiere al último elemento, -2 se refiere al segundo último y el negativo de la longitud de lista/tupla se refiere al primer elemento.
+vegetales = ('Tomate', 'Papa', 'Palta','Cebolla', 'Zanahoria')
+#              -5       -4        -3      -2           -1
+arti1 = vegetales[-4]
+print(arti1)
+arti2 = vegetales[-3]
+print(arti2)
+ulti_vege= vegetales[-1]
+print(ulti_vege)
