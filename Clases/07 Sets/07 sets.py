84ca64b67b36e3e33d76297d9d1cefdf48eb30f3
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2024-08-19 19:40:28 -0500

    Update 07 sets.py

---
+++ Clases/07 Sets/07 sets.py
@@ -0,0 +1,24 @@
+# ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━🎮
+# ****🤖Programador:Julia Burga Bracamonte******
+# **********************************************
+# ****🔒GitHub:https://github.com/JulianB0315 **    
+# ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━🏀
+#-------------------------------------------------------------------------------------------------------------------------------------------
+#Set es una colección de artículos.
+#Como ejemplo toma un conjunto matemática que se puede aplicar en Python,
+# un set viene a ser una colección de elementos distintos, desordenados y no indexados
+#En Python set se utiliza para almacenar elementos únicos 
+#Sabiendo eso pasemos a la programación de un set 
+#-------------------------------------------------------------------------------------------------------------------------------------------
+#Set vacio
+st=set()
+st={'item1', 'item2', 'item3', 'item4'}
+#Set lleno 
+frutas={'platano','naranja','manzana','limon'}
+#-------------------------------------------------------------------------------------------------------------------------------------------
+#Como ya sabemos crear un set ahora obtengamos la longitud de este mismo 
+#Usamos len() para contar la longitud de conjunto
+frutas={'platano','naranja','manzana','limon'}
+cuentaFrutas=len(frutas)
+print(cuentaFrutas)
+#-------------------------------------------------------------------------------------------------------------------------------------------
