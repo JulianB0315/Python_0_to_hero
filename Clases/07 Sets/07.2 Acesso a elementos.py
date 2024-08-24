09388ae3be12d9700b7aedea28fac7144b412262
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2024-08-24 16:53:23 -0500

    Create 07.2 Acesso a elementos.py

---
+++ Clases/07 Sets/07.2 Acesso a elementos.py
@@ -0,0 +1,11 @@
+#-------------------------------------------------------------------------------------------------------------------------------------------
+#Se puedes acceder a estos elementos por bucles pero dejemos eso para la clase de blucles
+#-------------------------------------------------------------------------------------------------------------------------------------------
+""""Comprobando un Articulo"""
+#Para comprobar si existe un elemento en una lista que utilizamos en operador de membresía.
+st={"item1","item2","item3","item4"}
+print("¿Estas el item3?",'item3'in st)
+#otro ejemplo
+frutas={"naranja","Manzana","Platano","Durazno"}
+print("mango"in frutas)
+#-------------------------------------------------------------------------------------------------------------------------------------------
