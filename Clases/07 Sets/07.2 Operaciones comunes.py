5a1df1373ce42dac81f5dce3f58433c0bf6147f0
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2025-01-04 20:14:44 -0500

    cambios de archivos

--- Clases/07 Sets/07.2 Operaciones comunes.py
+++ Clases/07 Sets/07.2 Operaciones comunes.py
@@ -1,4 +1,30 @@
 #-------------------------------------------------------------------------------------------------------------------------------------------
+#Se puedes acceder a estos elementos por bucles pero dejemos eso para la clase de blucles
+#-------------------------------------------------------------------------------------------------------------------------------------------
+""""Comprobando un Articulo"""
+#Para comprobar si existe un elemento en una lista que utilizamos en operador de membresía.
+st={"item1","item2","item3","item4"}
+print("¿Estas el item3?",'item3'in st)
+#otro ejemplo
+frutas={"naranja","Manzana","Platano","Durazno"}
+print("mango"in frutas)
+#-------------------------------------------------------------------------------------------------------------------------------------------
+#Ahora aprendamos como agregar elementos los “sets”
+#-------------------------------------------------------------------------------------------------------------------------------------------
+#Para agregar un elemento a un conjunto en Python, utilizamos el método add(). Este método permite incluir un solo elemento, y si el elemento ya está presente, no se agrega nuevamente.
+# Crear un conjunto
+mi_set = {1, 2, 3}
+# Agregar un elemento
+mi_set.add(4)
+# Imprimir el conjunto
+print(mi_set)  # Salida: {1, 2, 3, 4}
+# Agregar un elemento duplicado
+mi_set.add(2)
+print(mi_set)  # Salida: {1, 2, 3, 4} (sin cambios)
+#Para agregar múltiples elementos, utiliza el método update():
+mi_set.update([5, 6])
+print(mi_set)  # Salida: {1, 2, 3, 4, 5, 6}
+#-------------------------------------------------------------------------------------------------------------------------------------------
 #Para eliminar un elemento de un conjunto en Python, puedes usar los métodos remove(), discard(), o pop().
 #-------------------------------------------------------------------------------------------------------------------------------------------
 #remove(elemento): Elimina el elemento especificado. Si el elemento no está presente, se lanza un KeyError.
